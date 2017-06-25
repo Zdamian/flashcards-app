d12d2fb994008129394ef34c4070b2045d376780
Angular CLI <angular-cli@angular.io>
2017-06-25 11:26:31 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FlashcardsAppPage } from './app.po';
+
+describe('flashcards-app App', () => {
+  let page: FlashcardsAppPage;
+
+  beforeEach(() => {
+    page = new FlashcardsAppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
